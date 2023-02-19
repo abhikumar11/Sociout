e516b20aa5eca2c4628c6d0c0cfeff2e3a304ac4
abhikumar11 <abhishekkumar12verma@gmail.com>
2023-02-19 16:55:38 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 
-const express=require('express');
+import  express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path  from "path";
+import { fileURLToPath } from "url";
 
 const app=express();
 const port=5000;
+
+const _filename=fileURLToPath(import.meta.url);
+
 app.get("/",(req, res) => {
     res.send("<h1>Hello World</h1>");
 });
